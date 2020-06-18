42d3728979ace7682273254bb1133f117d9c7883
gans <alexandre@presslab.com.br>
2020-06-18 17:17:45 -0300

    Alterado o GenericGenerator criado pelo @neemias.souza para desacoplar de variáveis hardcoded

---
+++ src/main/java/br/com/codenation/desafio/annotation/CompoundPk.java
@@ -0,0 +1,9 @@
+package br.com.codenation.desafio.annotation;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface CompoundPk {
+
+}
