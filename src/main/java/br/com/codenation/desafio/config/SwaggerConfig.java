3b2625adc6ffc4d2516476fe9f2f7dc509621642
Alexandre Gans <alexandregans@gmail.com.br>
2020-06-29 16:14:50 -0300

    Adicionado a configuração do swagger e alterado o ci para java 1.8

---
+++ src/main/java/br/com/codenation/desafio/config/SwaggerConfig.java
@@ -0,0 +1,41 @@
+package br.com.codenation.desafio.config;
+
+import java.util.ArrayList;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket apis() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("br.com.codenation.desafio"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo())
+                .useDefaultResponseMessages(false);
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "Backend Squad3",
+                "API Rest",
+                "1.0",
+                "N/A",
+                null,
+                "MIT",
+                "N/A",
+                new ArrayList<>()
+        );
+    }
+}
