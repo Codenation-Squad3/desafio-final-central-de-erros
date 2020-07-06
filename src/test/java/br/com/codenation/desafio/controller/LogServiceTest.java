074a89af76b9043cd4736fd1b266ce5f58d97708
alexandre.pacheco <alexandrespmg@hotmail.com>
2020-07-06 18:45:56 -0300

    Banco de dados local para testes

--- src/test/java/br/com/codenation/desafio/controller/LogServiceTest.java
+++ src/test/java/br/com/codenation/desafio/controller/LogServiceTest.java
@@ -8,7 +8,7 @@ import java.util.UUID;
 
 import javax.transaction.Transactional;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
