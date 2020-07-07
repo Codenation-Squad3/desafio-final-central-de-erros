744cd96e294e5cc3ac769b751e4837965e16281d
alexandre.pacheco <alexandrespmg@hotmail.com>
2020-07-07 08:54:20 -0300

    Colocar valores autenticação em variavel de ambiente

--- src/main/java/br/com/codenation/desafio/config/AuthenticationServerConfiguration.java
+++ src/main/java/br/com/codenation/desafio/config/AuthenticationServerConfiguration.java
@@ -1,21 +1,33 @@
 package br.com.codenation.desafio.config;
 
-import br.com.codenation.desafio.constants.OAuthUser;
 import br.com.codenation.desafio.login.LoggedUser;
 import br.com.codenation.desafio.model.User;
 import br.com.codenation.desafio.repository.UserRepository;
+import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
+@Getter
 @Configuration
 @EnableAuthorizationServer
+@ConfigurationProperties(prefix = "authorization")
+@PropertySource("classpath:application.yml")
 public class AuthenticationServerConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Value("${email}")
+    private String OAUTH_USER_EMAIL;
+    @Value("${password}")
+    private String OAUTH_USER_PASSWORD;
+    private String OAUTH_USER_NAME = "Admin";
 
     @Bean
     public AuthenticationManager customAuthenticationManager() throws Exception {
@@ -26,9 +38,9 @@ public class AuthenticationServerConfiguration extends WebSecurityConfigurerAdap
     protected void configure(AuthenticationManagerBuilder auth, UserRepository userRepository) throws Exception {
         if (userRepository.count() == 0) {
             userRepository.save(User.builder()
-                    .email(OAuthUser.email)
-                    .password(passwordEncoder().encode(OAuthUser.password))
-                    .nome(OAuthUser.name)
+                    .email(OAUTH_USER_EMAIL)
+                    .password(passwordEncoder().encode(OAUTH_USER_PASSWORD))
+                    .nome(OAUTH_USER_NAME)
                     .build());
         }
         auth.userDetailsService(email -> userRepository.findByEmail(email)
--- src/main/java/br/com/codenation/desafio/config/AuthorizationServerConfig.java
+++ src/main/java/br/com/codenation/desafio/config/AuthorizationServerConfig.java
@@ -1,8 +1,11 @@
 package br.com.codenation.desafio.config;
 
-import br.com.codenation.desafio.constants.OAuthClient;
+import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -14,8 +17,19 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Aut
 
 @Configuration
 @EnableAuthorizationServer
+@ConfigurationProperties(prefix = "authorization.client")
+@PropertySource("classpath:application.yml")
+@Getter
 public class AuthorizationServerConfig  extends AuthorizationServerConfigurerAdapter {
 
+    @Value("${id}")
+    private String OAUTH_CLIENT_ID;
+    @Value("${secret}")
+    private String OAUTH_CLIENT_SECRET;
+    @Value("${grant-types}")
+    private String[] OAUTH_CLIENT_GRANT_TYPES;
+    @Value("${scopes}")
+    private String[] OAUTH_CLIENT_SCOPES;
     private static final int ACCESS_TOKEN_VALIDITY_IN_SECONDS = 60 * 20;
     private static final int REFRESH_TOKEN_VALIDITY_IN_SECONDS = 60 * 60;
 
@@ -33,10 +47,10 @@ public class AuthorizationServerConfig  extends AuthorizationServerConfigurerAda
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
         clients.inMemory()
-                .withClient(OAuthClient.clientId)
-                .secret(new BCryptPasswordEncoder().encode(OAuthClient.clientSecret))
-                .authorizedGrantTypes(OAuthClient.grantTypes)
-                .scopes(OAuthClient.scopes)
+                .withClient(OAUTH_CLIENT_ID)
+                .secret(new BCryptPasswordEncoder().encode(OAUTH_CLIENT_SECRET))
+                .authorizedGrantTypes(OAUTH_CLIENT_GRANT_TYPES)
+                .scopes(OAUTH_CLIENT_SCOPES)
                 .accessTokenValiditySeconds(ACCESS_TOKEN_VALIDITY_IN_SECONDS)
                 .refreshTokenValiditySeconds(REFRESH_TOKEN_VALIDITY_IN_SECONDS);
     }
